9975549e965c3eb366687105cc19e40f5849bb99
h24axeal <h24axeal@du.se>
2026-04-23 15:26:55 +0200

    fixat kartan

--- webmapp201/webmapp201/app.py
+++ webmapp201/webmapp201/app.py
@@ -1,11 +1,30 @@
-from flask import Flask, render_template
+from pathlib import Path
 
-app = Flask(__name__)
+from flask import Flask, render_template, send_from_directory
+
+BASE_DIR = Path(__file__).resolve().parents[2]
+APP_DIR = Path(__file__).resolve().parent
+
+app = Flask(
+    __name__,
+    static_folder=str(BASE_DIR / "static"),
+    static_url_path="/static",
+)
 
 @app.route('/')
 def index():
     # Detta laddar filen index.html från mappen 'templates'
     return render_template('index.html')
+
+
+@app.route('/src/<path:filename>')
+def src_files(filename):
+    return send_from_directory(APP_DIR / 'src', filename)
+
+
+@app.route('/dist/<path:filename>')
+def dist_files(filename):
+    return send_from_directory(APP_DIR / 'dist', filename)
 
 if __name__ == '__main__':
     app.run(debug=True)
